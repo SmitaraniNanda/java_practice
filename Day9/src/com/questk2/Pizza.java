2d417f9967c1f01cd3dc1d9b1aa1e63de8fa334e
Smita <smitan@questk2.com>
2025-02-18 11:07:16 +0530

    do

--- Day9/src/com/questk2/Pizza.java
+++ Day9/src/com/questk2/Pizza.java
@@ -7,19 +7,34 @@ class Pizza extends FoodItem {
     private String name;
     private double price;
 
+    /**
+     * Constructor to initialize a Pizza object.
+     *
+     * @param name  The name of the pizza.
+     * @param price The price of the pizza.
+     */
     public Pizza(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
+    /**
+     * Gets the name of the pizza.
+     *
+     * @return The name of the pizza.
+     */
     @Override
     public String name() {
         return name;
     }
 
+    /**
+     * Gets the price of the pizza.
+     *
+     * @return The price of the pizza.
+     */
     @Override
     public double price() {
         return price;
     }
-   
 }
